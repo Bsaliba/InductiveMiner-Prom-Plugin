434bb4130e3655802c7a6c95b220510d07fcb0f5
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2013-01-22 12:50:41 +0000

    added batch mining

---
+++ src/bPrime/model/ProcessTrees.java
@@ -0,0 +1,10 @@
+package bPrime.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.processmining.processtree.ProcessTree;
+
+public class ProcessTrees {
+	public List<ProcessTree> trees = new ArrayList<ProcessTree>();
+}
