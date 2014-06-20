3b9e8b7480aa1b51ca0806536ef8b6485fdcb3e7
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2014-06-20 14:35:09 +0000

    Consolidated plug-ins

--- src/org/processmining/plugins/InductiveMiner/plugins/IMPlaygroundProcessTree.java
+++ src/org/processmining/plugins/InductiveMiner/plugins/IMPlaygroundProcessTree.java
@@ -5,20 +5,16 @@ import javax.swing.JOptionPane;
 import org.deckfour.uitopia.api.event.TaskListener.InteractionResult;
 import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.UIPluginContext;
-import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.annotations.Plugin;
-import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.plugins.InductiveMiner.mining.MiningParameters;
 import org.processmining.plugins.InductiveMiner.mining.MiningParametersIMPlayground;
 import org.processmining.plugins.InductiveMiner.plugins.dialogs.IMiMiningDialog;
 import org.processmining.processtree.ProcessTree;
 
 @Plugin(name = "Mine Process Tree with Inductive Miner-playground", returnLabels = { "Process Tree" }, returnTypes = { ProcessTree.class }, parameterLabels = {
-		"Log" }, userAccessible = true)
+		"Log" }, userAccessible = false)
 public class IMPlaygroundProcessTree {
 	
-	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "S.J.J. Leemans", email = "s.j.j.leemans@tue.nl")
-	@PluginVariant(variantLabel = "Mine a Petri net, dialog", requiredParameterLabels = { 0 })
 	public ProcessTree mineGuiProcessTree(UIPluginContext context, XLog log) {
 		MiningParameters parameters = new MiningParametersIMPlayground();
 		IMiMiningDialog dialog = new IMiMiningDialog(log, parameters);
--- src/org/processmining/plugins/InductiveMiner/plugins/IMinPetriNet.java
+++ src/org/processmining/plugins/InductiveMiner/plugins/IMinPetriNet.java
@@ -3,15 +3,10 @@ package org.processmining.plugins.InductiveMiner.plugins;
 import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.PluginContext;
-import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginVariant;
-import org.processmining.models.graphbased.directed.petrinet.Petrinet;
-import org.processmining.models.semantics.petrinet.Marking;
 import org.processmining.plugins.InductiveMiner.mining.MiningParameters;
 import org.processmining.plugins.InductiveMiner.mining.MiningParametersIMi;
 
-@Plugin(name = "Mine Petri net with Inductive Miner-incompleteness", returnLabels = { "Petri net", "Initial marking", "final marking" }, returnTypes = {
-		Petrinet.class, Marking.class, Marking.class }, parameterLabels = { "Log" }, userAccessible = true)
 public class IMinPetriNet {
 	
 	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "S.J.J. Leemans", email = "s.j.j.leemans@tue.nl")
