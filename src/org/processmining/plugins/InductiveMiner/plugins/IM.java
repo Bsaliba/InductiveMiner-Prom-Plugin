d2a48abe2921c913c1110982820b7a6b11e49f44
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2014-10-16 11:41:24 +0000

    gui supports classifiers

--- src/org/processmining/plugins/InductiveMiner/plugins/IM.java
+++ src/org/processmining/plugins/InductiveMiner/plugins/IM.java
@@ -1,6 +1,10 @@
 package org.processmining.plugins.InductiveMiner.plugins;
 
+import java.util.List;
+
 import org.deckfour.uitopia.api.event.TaskListener.InteractionResult;
+import org.deckfour.xes.classification.XEventClassifier;
+import org.deckfour.xes.classification.XEventNameClassifier;
 import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.UIPluginContext;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
@@ -14,9 +18,8 @@ import org.processmining.plugins.InductiveMiner.plugins.dialogs.IMMiningDialog;
 import org.processmining.processtree.ProcessTree;
 
 public class IM {
-	
-	@Plugin(name = "Mine process tree with Inductive Miner", returnLabels = { "Process Tree" }, returnTypes = { ProcessTree.class }, parameterLabels = {
-	"Log" }, userAccessible = true)
+
+	@Plugin(name = "Mine process tree with Inductive Miner", returnLabels = { "Process Tree" }, returnTypes = { ProcessTree.class }, parameterLabels = { "Log" }, userAccessible = true)
 	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "S.J.J. Leemans", email = "s.j.j.leemans@tue.nl")
 	@PluginVariant(variantLabel = "Mine a Process Tree, dialog", requiredParameterLabels = { 0 })
 	public ProcessTree mineGuiProcessTree(UIPluginContext context, XLog log) {
@@ -27,9 +30,9 @@ public class IM {
 		}
 		return IMProcessTree.mineProcessTree(log, dialog.getMiningParameters());
 	}
-	
-	@Plugin(name = "Mine Petri net with Inductive Miner", returnLabels = { "Petri net", "Initial marking", "final marking" }, returnTypes = {
-			Petrinet.class, Marking.class, Marking.class }, parameterLabels = { "Log" }, userAccessible = true)
+
+	@Plugin(name = "Mine Petri net with Inductive Miner", returnLabels = { "Petri net", "Initial marking",
+			"final marking" }, returnTypes = { Petrinet.class, Marking.class, Marking.class }, parameterLabels = { "Log" }, userAccessible = true)
 	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "S.J.J. Leemans", email = "s.j.j.leemans@tue.nl")
 	@PluginVariant(variantLabel = "Mine a Process Tree, dialog", requiredParameterLabels = { 0 })
 	public Object[] mineGuiPetrinet(UIPluginContext context, XLog log) {
@@ -40,18 +43,29 @@ public class IM {
 		}
 		return IMPetriNet.minePetriNet(context, log, dialog.getMiningParameters());
 	}
-	
-	@Plugin(name = "Mine Process tree with Inductive Miner, with parameters", returnLabels = { "Process tree" }, returnTypes = {
-			ProcessTree.class }, parameterLabels = { "Log", "IM Parameters" }, userAccessible = false)
+
+	@Plugin(name = "Mine Process tree with Inductive Miner, with parameters", returnLabels = { "Process tree" }, returnTypes = { ProcessTree.class }, parameterLabels = {
+			"Log", "IM Parameters" }, userAccessible = false)
 	@PluginVariant(variantLabel = "Mine a Process Tree, parameters", requiredParameterLabels = { 0, 1 })
 	public static ProcessTree mineProcessTree(PluginContext context, XLog log, MiningParameters parameters) {
 		return IMProcessTree.mineProcessTree(log, parameters);
 	}
-	
-	@Plugin(name = "Mine Petri net with Inductive Miner, with parameters", returnLabels = { "Petri net", "Initial marking", "final marking" }, returnTypes = {
-			Petrinet.class, Marking.class, Marking.class }, parameterLabels = { "Log", "IM Parameters" }, userAccessible = false)
+
+	@Plugin(name = "Mine Petri net with Inductive Miner, with parameters", returnLabels = { "Petri net",
+			"Initial marking", "final marking" }, returnTypes = { Petrinet.class, Marking.class, Marking.class }, parameterLabels = {
+			"Log", "IM Parameters" }, userAccessible = false)
 	@PluginVariant(variantLabel = "Mine a Process Tree, parameters", requiredParameterLabels = { 0, 1 })
 	public static Object[] minePetriNet(PluginContext context, XLog log, MiningParameters parameters) {
 		return IMPetriNet.minePetriNet(context, log, parameters);
+	}
+
+	public static XEventClassifier[] getClassifiers(XLog log) {
+		List<XEventClassifier> logClassifiers = log.getClassifiers();
+		XEventClassifier[] result = new XEventClassifier[logClassifiers.size() + 1];
+		result[0] = new XEventNameClassifier();
+		for (int i = 0; i < logClassifiers.size(); i++) {
+			result[i+1] = logClassifiers.get(i);
+		}
+		return result;
 	}
 }
