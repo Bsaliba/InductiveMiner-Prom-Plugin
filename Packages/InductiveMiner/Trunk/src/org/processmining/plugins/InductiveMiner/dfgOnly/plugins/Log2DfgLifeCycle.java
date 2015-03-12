c3c037d752a388fa824483724dede3673082876e
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2015-03-12 14:45:24 +0000

    cloning IMEventIterator

--- Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/dfgOnly/plugins/Log2DfgLifeCycle.java
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/dfgOnly/plugins/Log2DfgLifeCycle.java
@@ -13,6 +13,7 @@ import org.processmining.plugins.InductiveMiner.dfgOnly.Dfg;
 import org.processmining.plugins.InductiveMiner.mining.MiningParameters;
 import org.processmining.plugins.InductiveMiner.mining.logs.IMLog2;
 import org.processmining.plugins.InductiveMiner.mining.logs.IMTrace2;
+import org.processmining.plugins.InductiveMiner.mining.logs.IMTrace2.IMEventIterator;
 
 public class Log2DfgLifeCycle {
 	@Plugin(name = "Convert log to directly-follows graph using lifecycle", returnLabels = { "Directly-follows graph" }, returnTypes = { Dfg.class }, parameterLabels = { "Log" }, userAccessible = true)
@@ -24,144 +25,206 @@ public class Log2DfgLifeCycle {
 		return log2Dfg(new IMLog2(log, MiningParameters.getDefaultClassifier()));
 	}
 
+	/**
+	 * Get a directly-follows graph from a log.
+	 * 
+	 * @param log
+	 * @return
+	 */
 	public static Dfg log2Dfg(IMLog2 log) {
-
-		//read log
 		Dfg dfg = new Dfg();
 		for (IMTrace2 trace : log) {
+			processTrace(log, dfg, trace);
+		}
+		return dfg;
+	}
 
-			//directly-follows relation
-			if (!trace.isEmpty()) {
-				int front = 0;
-				int back = 0;
+	private static void processTrace(IMLog2 log, Dfg dfg, IMTrace2 trace) {
+		if (trace.isEmpty()) {
+			return;
+		}
 
-				MultiSet<XEventClass> openActivityOccurrencesAtFront = new MultiSet<XEventClass>();
-				XEvent eventFront = trace.get(front);
-				if (log.isStart(eventFront)) {
-					openActivityOccurrencesAtFront.add(log.classify(eventFront));
+		//directly-follows relation
+		processTraceDirectlyFollows(log, dfg, trace);
+		
+		processDirectlyFollows(log, dfg, trace);
+
+		//parallelism
+		processParallel(log, dfg, trace);
+
+		//start/end activities
+		processStartEnd(log, dfg, trace);
+	}
+
+	private static void processStartEnd(IMLog2 log, Dfg dfg, IMTrace2 trace) {
+		boolean activityOccurrenceCompleted = false;
+		MultiSet<XEventClass> activityOccurrencesEndedSinceLastStart = new MultiSet<>();
+		MultiSet<XEventClass> openActivityOccurrences = new MultiSet<XEventClass>();
+		for (XEvent event : trace) {
+			XEventClass activity = log.classify(event);
+
+			if (log.isStart(event)) {
+				openActivityOccurrences.add(activity);
+				if (!activityOccurrenceCompleted) {
+					//no activity occurrence has been completed yet. Add to start events.
+					dfg.addStartActivity(activity, 1);
 				}
+				activityOccurrencesEndedSinceLastStart = new MultiSet<>();
+			} else if (log.isComplete(event)) {
+				//complete event
+				if (openActivityOccurrences.contains(activity)) {
+					//this activity occurrence was open; close it
+					openActivityOccurrences.add(activity, -1);
+					activityOccurrencesEndedSinceLastStart.add(activity);
+				} else {
+					//next front is non-started but complete
 
-				while (front < trace.size() - 1) {
-					XEvent nextFrontEvent = trace.get(front + 1);
-					XEventClass nextFrontClass = log.classify(nextFrontEvent);
-
-					//see whether this event was opened before
-					if (log.isStart(nextFrontEvent)) {
-						//we are starting an activity occurrence, add it to the list of open activity occurrences. 
-						openActivityOccurrencesAtFront.add(nextFrontClass);
-					} else {
-						//complete event
-						if (openActivityOccurrencesAtFront.contains(nextFrontClass)) {
-							//this activity occurrence was open; close it
-							openActivityOccurrencesAtFront.remove(nextFrontClass, 1);
-						} else {
-							//next front is non-started but complete
-
-							if (log.isStart(nextFrontEvent)) {
-								//we are adding a start event; add all complete activities from [back:front] to the directly-follows graph.
-								XEventClass to = nextFrontClass;
-								for (XEvent from : trace.subList(back, front + 1)) {
-									if (log.isComplete(from)) {
-										XEventClass fromClass = log.classify(from);
-										dfg.addDirectlyFollowsEdge(fromClass, to, 1);
-									}
-								}
-							}
-
-							//progress back and front
-							front++;
-							back = front;
-							continue;
-						}
+					if (!activityOccurrenceCompleted) {
+						//no activity occurrence has been completed yet. Add to start events.
+						dfg.addStartActivity(activity, 1);
 					}
+					activityOccurrenceCompleted = true;
 
-					if (log.isComplete(nextFrontEvent) && containsActivity(log, nextFrontClass, trace, back, front)) {
-						//we cannot progress front, so progress back
-						back++;
-					} else {
-						//progress front
+					activityOccurrencesEndedSinceLastStart = new MultiSet<>();
+					activityOccurrencesEndedSinceLastStart.add(activity);
+				}
+			}
 
-						if (log.isStart(nextFrontEvent)) {
-							//we are adding a start event; add all complete activities from [back:front] to the directly-follows graph.
-							XEventClass to = log.classify(nextFrontEvent);
-							for (XEvent from : trace.subList(back, front + 1)) {
-								if (log.isComplete(from)) {
-									XEventClass fromClass = log.classify(from);
-									dfg.addDirectlyFollowsEdge(fromClass, to, 1);
-								}
-							}
-						}
+			activityOccurrenceCompleted = activityOccurrenceCompleted || log.isComplete(event);
+		}
+		dfg.getEndActivities().addAll(activityOccurrencesEndedSinceLastStart);
+	}
 
-						//progress front
-						front++;
-					}
+	private static void processParallel(IMLog2 log, Dfg dfg, IMTrace2 trace) {
+		MultiSet<XEventClass> openActivityOccurrences = new MultiSet<XEventClass>();
+		for (XEvent event : trace) {
+			XEventClass eventClass = log.classify(event);
+
+			if (log.isStart(event)) {
+				//this is a start event
+				openActivityOccurrences.add(eventClass);
+			} else {
+				//this is an end event
+				openActivityOccurrences.remove(eventClass, 1);
+
+				//this activity occurrence is parallel to all open activity occurrences
+				for (XEventClass eventClass2 : openActivityOccurrences) {
+					dfg.addParallelEdge(eventClass, eventClass2, openActivityOccurrences.getCardinalityOf(eventClass2));
+				}
+			}
+		}
+	}
+
+	private static void processDirectlyFollows(IMLog2 log, Dfg dfg, IMTrace2 trace) {
+		IMEventIterator itCurrent = trace.iterator();
+		IMEventIterator itLastStart = null;
+		IMEventIterator itLastComplete = null;
+		MultiSet<XEventClass> openActivityInstances = new MultiSet<>();
+
+		while (itCurrent.hasNext()) {
+			XEvent event = itCurrent.next();
+			XEventClass activity = log.classify(event);
+			System.out.println("read " + activity);
+
+			//this is a start event if the log says so, or if we see a complete without corresponding preceding start event. 
+			boolean isStartEvent = log.isStart(event) || !openActivityInstances.contains(activity);
+			boolean isCompleteEvent = log.isComplete(event);
+			System.out.println(" start " + isStartEvent + ", complete " + isCompleteEvent);
+			
+			if (isStartEvent && itLastStart != null) {
+				//the predecessors of this node are all from lastStart till here
+				for (XEvent predecessor : itLastStart.getUntil(itCurrent)) {
+//					if (log.isComplete(predecessor)) {
+						XEventClass predecessorActivity = log.classify(predecessor);
+						System.out.println(" predecessor " + predecessorActivity);
+//					}
 				}
 			}
 			
-			//parallelism
-			{
-				MultiSet<XEventClass> openActivityOccurrences = new MultiSet<XEventClass>();
-				for (XEvent event : trace) {
-					XEventClass eventClass = log.classify(event);
-					
-					if (log.isStart(event)) {
-						//this is a start event
-						openActivityOccurrences.add(eventClass);
-					} else {
-						//this is an end event
-						openActivityOccurrences.remove(eventClass, 1);
-						
-						//this activity occurrence is parallel to all open activity occurrences
-						for (XEventClass eventClass2 : openActivityOccurrences) {
-							dfg.addParallelEdge(eventClass, eventClass2, openActivityOccurrences.getCardinalityOf(eventClass2));
-						}
-					}
-				}
-				
+			if (isCompleteEvent) {
+				itLastComplete = itCurrent.clone();
+			}
+			if (isStartEvent) {
+				itLastStart = itCurrent.clone();
 			}
 
-			//start/end activities
-			{
-				boolean activityOccurrenceCompleted = false;
-				MultiSet<XEventClass> activityOccurrencesEndedSinceLastStart = new MultiSet<>();
-				MultiSet<XEventClass> openActivityOccurrences = new MultiSet<XEventClass>();
-				for (XEvent event : trace) {
-					XEventClass activity = log.classify(event);
-
-					if (log.isStart(event)) {
-						openActivityOccurrences.add(activity);
-						if (!activityOccurrenceCompleted) {
-							//no activity occurrence has been completed yet. Add to start events.
-							dfg.addStartActivity(activity, 1);
-						}
-						activityOccurrencesEndedSinceLastStart = new MultiSet<>();
-					} else if (log.isComplete(event)) {
-						//complete event
-						if (openActivityOccurrences.contains(activity)) {
-							//this activity occurrence was open; close it
-							openActivityOccurrences.add(activity, -1);
-							activityOccurrencesEndedSinceLastStart.add(activity);
-						} else {
-							//next front is non-started but complete
-
-							if (!activityOccurrenceCompleted) {
-								//no activity occurrence has been completed yet. Add to start events.
-								dfg.addStartActivity(activity, 1);
-							}
-							activityOccurrenceCompleted = true;
-
-							activityOccurrencesEndedSinceLastStart = new MultiSet<>();
-							activityOccurrencesEndedSinceLastStart.add(activity);
-						}
-					}
-
-					activityOccurrenceCompleted = activityOccurrenceCompleted || log.isComplete(event);
-				}
-				dfg.getEndActivities().addAll(activityOccurrencesEndedSinceLastStart);
+			//update the open activity instances
+			if (isCompleteEvent && !isStartEvent) {
+				//if this ends an activity instance (and it was open already), remove it 
+				openActivityInstances.remove(activity);
+			}
+			if (isStartEvent && !isCompleteEvent) {
+				//if this starts an activity instance (and does not immediately close it), it is left open for now
+				openActivityInstances.add(activity);
 			}
 		}
+	}
 
-		return dfg;
+	private static void processTraceDirectlyFollows(IMLog2 log, Dfg dfg, IMTrace2 trace) {
+		int front = 0;
+		int back = 0;
+
+		MultiSet<XEventClass> openActivityOccurrencesAtFront = new MultiSet<XEventClass>();
+		XEvent eventFront = trace.get(front);
+		if (log.isStart(eventFront)) {
+			openActivityOccurrencesAtFront.add(log.classify(eventFront));
+		}
+
+		while (front < trace.size() - 1) {
+			XEvent nextFrontEvent = trace.get(front + 1);
+			XEventClass nextFrontClass = log.classify(nextFrontEvent);
+
+			//see whether this event was opened before
+			if (log.isStart(nextFrontEvent)) {
+				//we are starting an activity occurrence, add it to the list of open activity occurrences. 
+				openActivityOccurrencesAtFront.add(nextFrontClass);
+			} else {
+				//complete event
+				if (openActivityOccurrencesAtFront.contains(nextFrontClass)) {
+					//this activity occurrence was open; close it
+					openActivityOccurrencesAtFront.remove(nextFrontClass, 1);
+				} else {
+					//next front is non-started but complete
+
+					if (log.isStart(nextFrontEvent)) {
+						//we are adding a start event; add all complete activities from [back:front] to the directly-follows graph.
+						XEventClass to = nextFrontClass;
+						for (XEvent from : trace.subList(back, front + 1)) {
+							if (log.isComplete(from)) {
+								XEventClass fromClass = log.classify(from);
+								dfg.addDirectlyFollowsEdge(fromClass, to, 1);
+							}
+						}
+					}
+
+					//progress back and front
+					front++;
+					back = front;
+					continue;
+				}
+			}
+
+			if (log.isComplete(nextFrontEvent) && containsActivity(log, nextFrontClass, trace, back, front)) {
+				//we cannot progress front, so progress back
+				back++;
+			} else {
+				//progress front
+
+				if (log.isStart(nextFrontEvent)) {
+					//we are adding a start event; add all complete activities from [back:front] to the directly-follows graph.
+					XEventClass to = log.classify(nextFrontEvent);
+					for (XEvent from : trace.subList(back, front + 1)) {
+						if (log.isComplete(from)) {
+							XEventClass fromClass = log.classify(from);
+							dfg.addDirectlyFollowsEdge(fromClass, to, 1);
+						}
+					}
+				}
+
+				//progress front
+				front++;
+			}
+		}
 	}
 
 	private static boolean containsActivity(IMLog2 log, XEventClass eventClass, IMTrace2 trace, int back, int front) {
