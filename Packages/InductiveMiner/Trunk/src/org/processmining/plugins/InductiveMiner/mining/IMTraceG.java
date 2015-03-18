2f2819746f812bdd540975bb948d44eb2345bf6d
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2015-03-18 11:20:07 +0000

    remove old IMLog, IMTrace and IMTraceG

--- Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/IMTraceG.java
+++
@@ -1,9 +0,0 @@
-package org.processmining.plugins.InductiveMiner.mining;
-
-import java.util.ArrayList;
-
-public class IMTraceG<X> extends ArrayList<X> {
-
-	private static final long serialVersionUID = 3273097508614018458L;
-
-}
