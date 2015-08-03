4c0c2aa138eaa34e7270b83b6951ad90448e94e0
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2015-08-03 10:48:37 +0000

    added option to apply new noise filtering to directly-follows graphs.

--- Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/dfgOnly/plugins/dialogs/XLog2DfgDialog.java
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/dfgOnly/plugins/dialogs/XLog2DfgDialog.java
@@ -1,18 +1,26 @@
 package org.processmining.plugins.InductiveMiner.dfgOnly.plugins.dialogs;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.deckfour.xes.classification.XEventClassifier;
 import org.deckfour.xes.model.XLog;
 import org.processmining.plugins.InductiveMiner.Classifiers;
 import org.processmining.plugins.InductiveMiner.Classifiers.ClassifierWrapper;
 import org.processmining.plugins.InductiveMiner.dfgOnly.log2logInfo.IMLog2IMLogInfo;
+import org.processmining.plugins.InductiveMiner.dfgOnly.log2logInfo.IMLog2IMLogInfoBucketFilter;
 import org.processmining.plugins.InductiveMiner.dfgOnly.log2logInfo.IMLog2IMLogInfoDefault;
 import org.processmining.plugins.InductiveMiner.dfgOnly.log2logInfo.IMLog2IMLogInfoLifeCycle;
 
@@ -22,11 +30,16 @@ public class XLog2DfgDialog extends JPanel {
 
 	private static final long serialVersionUID = -3128018697672831767L;
 	private final JComboBox<Variant> variantCombobox;
+	private final JLabel noiseLabel;
+	private final JSlider noiseSlider;
+	private final JLabel noiseValue;
 	private final JComboBox<ClassifierWrapper> classifierCombobox;
 
 	public abstract class Variant {
 		@Override
 		public abstract String toString();
+
+		public abstract boolean hasNoise();
 
 		public abstract IMLog2IMLogInfo getIMLog2IMLogInfo();
 	}
@@ -35,6 +48,10 @@ public class XLog2DfgDialog extends JPanel {
 
 		public String toString() {
 			return "use event classes - (IMd, IMiD, IMinD)";
+		}
+
+		public boolean hasNoise() {
+			return false;
 		}
 
 		public IMLog2IMLogInfo getIMLog2IMLogInfo() {
@@ -48,21 +65,29 @@ public class XLog2DfgDialog extends JPanel {
 			return "use event classes and life cycles - (IMdLc, IMiDlc, IMinDlc)";
 		}
 
+		public boolean hasNoise() {
+			return false;
+		}
+
 		public IMLog2IMLogInfo getIMLog2IMLogInfo() {
 			return new IMLog2IMLogInfoLifeCycle();
 		}
 	}
-	
-//	public class VariantFilterNoise extends Variant {
-//
-//		public String toString() {
-//			return "use event classes and life cycles & filter infrequent behaviour";
-//		}
-//
-//		public IMLog2IMLogInfo getIMLog2IMLogInfo() {
-//			return new IMLog2IMLogInfoBucketFilter();
-//		}
-//	}
+
+	public class VariantFilterInfrequentBehaviour extends Variant {
+
+		public String toString() {
+			return "use event classes and life cycles & filter infrequent behaviour";
+		}
+
+		public boolean hasNoise() {
+			return true;
+		}
+
+		public IMLog2IMLogInfo getIMLog2IMLogInfo() {
+			return new IMLog2IMLogInfoBucketFilter(getNoiseThreshold());
+		}
+	}
 
 	@SuppressWarnings("unchecked")
 	public XLog2DfgDialog(XLog log) {
@@ -72,28 +97,91 @@ public class XLog2DfgDialog extends JPanel {
 
 		setLayout(new GridBagLayout());
 
-		//algorithm
-		final JLabel variantLabel = factory.createLabel("Variant");
+		//variant
 		{
-			GridBagConstraints cVariantLabel = new GridBagConstraints();
-			cVariantLabel.gridx = 0;
-			cVariantLabel.gridy = gridy;
-			cVariantLabel.weightx = 0.4;
-			cVariantLabel.anchor = GridBagConstraints.NORTHWEST;
-			add(variantLabel, cVariantLabel);
+			final JLabel variantLabel = factory.createLabel("Variant");
+			{
+				GridBagConstraints cVariantLabel = new GridBagConstraints();
+				cVariantLabel.gridx = 0;
+				cVariantLabel.gridy = gridy;
+				cVariantLabel.weightx = 0.4;
+				cVariantLabel.anchor = GridBagConstraints.NORTHWEST;
+				add(variantLabel, cVariantLabel);
+			}
+
+			variantCombobox = factory.createComboBox(new Variant[] { new VariantDefault(), new VariantLifeCycle(),
+					new VariantFilterInfrequentBehaviour() });
+			{
+				GridBagConstraints cVariantCombobox = new GridBagConstraints();
+				cVariantCombobox.gridx = 1;
+				cVariantCombobox.gridy = gridy;
+				cVariantCombobox.anchor = GridBagConstraints.NORTHWEST;
+				cVariantCombobox.fill = GridBagConstraints.HORIZONTAL;
+				cVariantCombobox.weightx = 0.6;
+				add(variantCombobox, cVariantCombobox);
+				variantCombobox.setSelectedIndex(0);
+			}
+		}
+		
+		gridy++;
+
+		//spacer
+		{
+			JLabel spacer = factory.createLabel(" ");
+			GridBagConstraints cSpacer = new GridBagConstraints();
+			cSpacer.gridx = 0;
+			cSpacer.gridy = gridy;
+			cSpacer.anchor = GridBagConstraints.WEST;
+			add(spacer, cSpacer);
 		}
 
-		variantCombobox = factory.createComboBox(new Variant[] { new VariantDefault(), new VariantLifeCycle()
-				 });
+		gridy++;
+
+		//noise threshold
 		{
-			GridBagConstraints cVariantCombobox = new GridBagConstraints();
-			cVariantCombobox.gridx = 1;
-			cVariantCombobox.gridy = gridy;
-			cVariantCombobox.anchor = GridBagConstraints.NORTHWEST;
-			cVariantCombobox.fill = GridBagConstraints.HORIZONTAL;
-			cVariantCombobox.weightx = 0.6;
-			add(variantCombobox, cVariantCombobox);
-			variantCombobox.setSelectedIndex(0);
+			noiseLabel = factory.createLabel("Noise threshold");
+			{
+				GridBagConstraints cNoiseLabel = new GridBagConstraints();
+				cNoiseLabel.gridx = 0;
+				cNoiseLabel.gridy = gridy;
+				cNoiseLabel.anchor = GridBagConstraints.WEST;
+				add(noiseLabel, cNoiseLabel);
+				noiseLabel.setVisible(false);
+			}
+
+			noiseSlider = factory.createSlider(SwingConstants.HORIZONTAL);
+			{
+				noiseSlider.setMinimum(0);
+				noiseSlider.setMaximum(1000);
+				noiseSlider.setValue((int) (getNoiseThreshold() * 1000));
+				GridBagConstraints cNoiseSlider = new GridBagConstraints();
+				cNoiseSlider.gridx = 1;
+				cNoiseSlider.gridy = gridy;
+				cNoiseSlider.fill = GridBagConstraints.HORIZONTAL;
+				add(noiseSlider, cNoiseSlider);
+				noiseSlider.setVisible(false);
+			}
+
+			noiseValue = factory.createLabel(String.format("%.2f", getNoiseThreshold()));
+			{
+				GridBagConstraints cNoiseValue = new GridBagConstraints();
+				cNoiseValue.gridx = 2;
+				cNoiseValue.gridy = gridy;
+				add(noiseValue, cNoiseValue);
+				noiseValue.setVisible(false);
+			}
+		}
+
+		gridy++;
+
+		//spacer
+		{
+			JLabel spacer = factory.createLabel(" ");
+			GridBagConstraints cSpacer = new GridBagConstraints();
+			cSpacer.gridx = 0;
+			cSpacer.gridy = gridy;
+			cSpacer.anchor = GridBagConstraints.WEST;
+			add(spacer, cSpacer);
 		}
 
 		gridy++;
@@ -119,7 +207,7 @@ public class XLog2DfgDialog extends JPanel {
 			cClassifiers.weightx = 0.6;
 			add(classifierCombobox, cClassifiers);
 		}
-		
+
 		gridy++;
 
 		{
@@ -129,13 +217,44 @@ public class XLog2DfgDialog extends JPanel {
 			gbcFiller.fill = GridBagConstraints.BOTH;
 			add(Box.createGlue(), gbcFiller);
 		}
+
+		variantCombobox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Variant variant = (Variant) variantCombobox.getSelectedItem();
+				if (variant.hasNoise()) {
+					noiseValue.setText(String.format("%.2f", getNoiseThreshold()));
+				} else {
+					int width = noiseValue.getWidth();
+					int height = noiseValue.getHeight();
+					noiseValue.setText("  ");
+					noiseValue.setPreferredSize(new Dimension(width, height));
+				}
+
+				noiseLabel.setVisible(variant.hasNoise());
+				noiseSlider.setVisible(variant.hasNoise());
+				noiseValue.setVisible(variant.hasNoise());
+			}
+		});
+		
+		noiseSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				noiseValue.setText(String.format("%.2f", getNoiseThreshold()));
+			}
+		});
 	}
 
 	public XEventClassifier getClassifier() {
 		return ((ClassifierWrapper) classifierCombobox.getSelectedItem()).classifier;
 	}
-	
+
 	public IMLog2IMLogInfo getIMLog2IMLogInfo() {
 		return ((Variant) variantCombobox.getSelectedItem()).getIMLog2IMLogInfo();
+	}
+
+	public double getNoiseThreshold() {
+		if (noiseSlider == null) {
+			return 0.2;
+		}
+		return noiseSlider.getValue() / 1000.0;
 	}
 }
