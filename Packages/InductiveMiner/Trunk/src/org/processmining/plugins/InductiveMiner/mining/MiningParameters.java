17e8965d2cd78f454a0e771f87b79bf72d99a54a
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2015-08-19 14:53:18 +0000

    removed commented-out line of code

--- Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/MiningParameters.java
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/MiningParameters.java
@@ -43,7 +43,6 @@ public abstract class MiningParameters {
 	}
 
 	public static XEventClassifier getDefaultClassifier() {
-		//return new XEventAndClassifier(new XEventNameClassifier(), new XEventLifeTransClassifier());
 		return new XEventNameClassifier();
 	}
 
