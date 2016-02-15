bb3f562a3a78493d7353e5e0039bb617f7c589ec
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2016-02-15 10:58:24 +0000

    using a temporary thesis miner

---
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/MiningParametersThesisIM.java
@@ -0,0 +1,52 @@
+package org.processmining.plugins.InductiveMiner.mining;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.processmining.plugins.InductiveMiner.dfgOnly.log2logInfo.IMLog2IMLogInfoDefault;
+import org.processmining.plugins.InductiveMiner.mining.baseCases.BaseCaseFinder;
+import org.processmining.plugins.InductiveMiner.mining.baseCases.BaseCaseFinderIM;
+import org.processmining.plugins.InductiveMiner.mining.cuts.CutFinder;
+import org.processmining.plugins.InductiveMiner.mining.cuts.IM.CutFinderIMExclusiveChoice;
+import org.processmining.plugins.InductiveMiner.mining.cuts.IM.CutFinderIMLoop;
+import org.processmining.plugins.InductiveMiner.mining.cuts.IM.CutFinderIMParallel;
+import org.processmining.plugins.InductiveMiner.mining.cuts.IM.CutFinderIMSequence;
+import org.processmining.plugins.InductiveMiner.mining.cuts.IMlc.CutFinderIMlcInterleaved;
+import org.processmining.plugins.InductiveMiner.mining.fallthrough.FallThrough;
+import org.processmining.plugins.InductiveMiner.mining.fallthrough.FallThroughFlower;
+import org.processmining.plugins.InductiveMiner.mining.fallthrough.FallThroughSingleParallel;
+import org.processmining.plugins.InductiveMiner.mining.fallthrough.FallThroughTauLoop;
+import org.processmining.plugins.InductiveMiner.mining.logSplitter.LogSplitterIMi;
+import org.processmining.plugins.InductiveMiner.mining.postprocessor.PostProcessor;
+import org.processmining.plugins.InductiveMiner.mining.postprocessor.PostProcessorInterleaved;
+
+public class MiningParametersThesisIM extends MiningParameters {
+	
+	public MiningParametersThesisIM() {
+	setLog2LogInfo(new IMLog2IMLogInfoDefault());
+	
+		setBaseCaseFinders(new ArrayList<BaseCaseFinder>(Arrays.asList(
+				new BaseCaseFinderIM()
+				)));
+		
+		setCutFinder(new ArrayList<CutFinder>(Arrays.asList(
+				new CutFinderIMExclusiveChoice(),
+				new CutFinderIMSequence(),
+				new CutFinderIMlcInterleaved(),
+				new CutFinderIMLoop(),
+				new CutFinderIMParallel()
+				)));
+		
+		setLogSplitter(new LogSplitterIMi());
+		
+		setFallThroughs(new ArrayList<FallThrough>(Arrays.asList(
+				new FallThroughSingleParallel(true),
+				new FallThroughTauLoop(false),
+				new FallThroughFlower()
+				)));
+		
+		setPostProcessors(new ArrayList<PostProcessor>(Arrays.asList(
+				new PostProcessorInterleaved()
+				)));
+	}
+}
