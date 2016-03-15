0b09c1f122a230b23218442a1b521f5e297038a3
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2016-03-15 11:27:53 +0000

    improved javadoc on graphs

--- Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/graphs/GraphFactory.java
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/graphs/GraphFactory.java
@@ -1,9 +1,7 @@
 package org.processmining.plugins.InductiveMiner.graphs;
 
-
-
 public class GraphFactory {
-	
+
 	public static <V> Graph<V> create(Class<?> clazz, int initialSize) {
 		return new GraphImplLinearEdge<V>(clazz);
 	}
