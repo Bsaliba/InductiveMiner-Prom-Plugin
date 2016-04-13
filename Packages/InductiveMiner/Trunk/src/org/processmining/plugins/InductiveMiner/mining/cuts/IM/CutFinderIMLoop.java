94b632b68a9b3d5fc7bf317bade022950e93cf12
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2016-04-13 12:21:08 +0000

    completed the loop transition

--- Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/cuts/IM/CutFinderIMLoop.java
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/cuts/IM/CutFinderIMLoop.java
@@ -1,14 +1,9 @@
 package org.processmining.plugins.InductiveMiner.mining.cuts.IM;
 
-import gnu.trove.map.TIntObjectMap;
-import gnu.trove.map.hash.THashMap;
-import gnu.trove.map.hash.TIntObjectHashMap;
-import gnu.trove.set.hash.THashSet;
+import gnu.trove.set.TIntSet;
+import gnu.trove.set.hash.TIntHashSet;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.deckfour.xes.classification.XEventClass;
@@ -16,7 +11,6 @@ import org.processmining.plugins.InductiveMiner.dfgOnly.Dfg;
 import org.processmining.plugins.InductiveMiner.dfgOnly.DfgMinerState;
 import org.processmining.plugins.InductiveMiner.dfgOnly.dfgCutFinder.DfgCutFinder;
 import org.processmining.plugins.InductiveMiner.graphs.Components;
-import org.processmining.plugins.InductiveMiner.graphs.Graph;
 import org.processmining.plugins.InductiveMiner.mining.IMLogInfo;
 import org.processmining.plugins.InductiveMiner.mining.MinerState;
 import org.processmining.plugins.InductiveMiner.mining.cuts.Cut;
@@ -84,18 +78,47 @@ public class CutFinderIMLoop implements CutFinder, DfgCutFinder {
 		 * each sub-component belongs to the body or the redo.
 		 */
 
-		//		//make a list of sub-start and sub-endactivities
-		//		TIntSet subStartActivities = new TIntHashSet();
-		//		TIntSet subEndActivities = new TIntHashSet();
-		//		for (long edgeIndex : dfg.getDirectlyFollowsEdges()) {
-		//			int source = dfg.getDirectlyFollowsEdgeSourceIndex(edgeIndex);
-		//			int target = dfg.getDirectlyFollowsEdgeTargetIndex(edgeIndex);
-		//
-		//			if (!components.areInSameComponent(source, target)) {
-		//				subEndActivities.add(source);
-		//				subStartActivities.add(target);
-		//			}
-		//		}
+		//make a list of sub-start and sub-endactivities
+		TIntSet subStartActivities = new TIntHashSet();
+		TIntSet subEndActivities = new TIntHashSet();
+		for (long edgeIndex : dfg.getDirectlyFollowsEdges()) {
+			int source = dfg.getDirectlyFollowsEdgeSourceIndex(edgeIndex);
+			int target = dfg.getDirectlyFollowsEdgeTargetIndex(edgeIndex);
+
+			if (!components.areInSameComponent(source, target)) {
+				//target is an sub-end activity and source is a sub-start activity
+				subEndActivities.add(source);
+				subStartActivities.add(target);
+			}
+		}
+
+		//a sub-end activity of a redo should have connections to all start activities
+		for (int subEndActivity : subEndActivities.toArray()) {
+			for (int startActivity : dfg.getStartActivityIndices()) {
+				if (components.areInSameComponent(subEndActivity, startActivity)) {
+					//this subEndActivity is already in the body
+					break;
+				}
+				if (!dfg.containsDirectlyFollowsEdge(subEndActivity, startActivity)) {
+					components.mergeComponentsOf(subEndActivity, startActivity);
+					break;
+				}
+			}
+		}
+
+		//a sub-start activity of a redo should be connections from all end activities
+		for (int subStartActivity : subStartActivities.toArray()) {
+			for (int endActivity : dfg.getEndActivityIndices()) {
+				if (components.areInSameComponent(subStartActivity, endActivity)) {
+					//this subStartActivity is already in the body
+					break;
+				}
+				if (!dfg.containsDirectlyFollowsEdge(endActivity, subStartActivity)) {
+					components.mergeComponentsOf(subStartActivity, endActivity);
+					break;
+				}
+			}
+		}
 
 		//put the start and end activity component first
 		List<Set<XEventClass>> partition = components.getComponents();
@@ -110,156 +133,6 @@ public class CutFinderIMLoop implements CutFinder, DfgCutFinder {
 		}
 
 		return new Cut(Operator.loop, partition);
-	}
-
-	public static Cut findCut(Dfg dfg) {
-		//initialise the start and end activities as a connected component
-		Map<XEventClass, Integer> connectedComponents = new THashMap<XEventClass, Integer>();
-		for (XEventClass startActivity : dfg.getStartActivities()) {
-			connectedComponents.put(startActivity, 0);
-		}
-		for (XEventClass endActivity : dfg.getEndActivities()) {
-			connectedComponents.put(endActivity, 0);
-		}
-
-		//find the other connected components
-		Integer ccs = 1;
-		for (XEventClass node : dfg.getDirectlyFollowsGraph().getVertices()) {
-			if (!connectedComponents.containsKey(node)) {
-				labelConnectedComponents(dfg.getDirectlyFollowsGraph(), node, connectedComponents, ccs);
-				ccs += 1;
-			}
-		}
-
-		//find the start activities of each component
-		TIntObjectMap<Set<XEventClass>> subStartActivities = new TIntObjectHashMap<Set<XEventClass>>();
-		for (Integer cc = 0; cc < ccs; cc++) {
-			subStartActivities.put(cc, new THashSet<XEventClass>());
-		}
-		for (XEventClass node : dfg.getDirectlyFollowsGraph().getVertices()) {
-			Integer cc = connectedComponents.get(node);
-			for (long edge : dfg.getDirectlyFollowsGraph().getIncomingEdgesOf(node)) {
-				if (cc != connectedComponents.get(dfg.getDirectlyFollowsGraph().getEdgeSource(edge))) {
-					//this is a sub-start activity
-					Set<XEventClass> start = subStartActivities.get(cc);
-					start.add(node);
-					subStartActivities.put(cc, start);
-				}
-			}
-		}
-
-		//find the end activities of each component
-		TIntObjectMap<Set<XEventClass>> subEndActivities = new TIntObjectHashMap<Set<XEventClass>>();
-		for (Integer cc = 0; cc < ccs; cc++) {
-			subEndActivities.put(cc, new THashSet<XEventClass>());
-		}
-		for (XEventClass node : dfg.getDirectlyFollowsGraph().getVertices()) {
-			Integer cc = connectedComponents.get(node);
-			for (long edge : dfg.getDirectlyFollowsGraph().getOutgoingEdgesOf(node)) {
-				if (cc != connectedComponents.get(dfg.getDirectlyFollowsGraph().getEdgeTarget(edge))) {
-					//this is a sub-end activity
-					Set<XEventClass> end = subEndActivities.get(cc);
-					end.add(node);
-					subEndActivities.put(cc, end);
-				}
-			}
-		}
-
-		//initialise the candidates
-		Boolean[] candidates = new Boolean[ccs];
-		//the start and end activities are no candidates
-		candidates[0] = false;
-		for (int i = 1; i < ccs; i++) {
-			candidates[i] = true;
-		}
-
-		//exclude all candidates that are reachable from the start activities (that are not an end activity)
-		for (int startActivity : dfg.getStartActivityIndices()) {
-			if (!dfg.isEndActivity(startActivity)) {
-				for (long edge : dfg.getDirectlyFollowsGraph().getOutgoingEdgesOf(startActivity)) {
-					candidates[connectedComponents.get(dfg.getDirectlyFollowsGraph().getEdgeTarget(edge))] = false;
-				}
-			}
-		}
-
-		//exclude all candidates that can reach an end activity (which is not a start activity)
-		for (int endActivity : dfg.getEndActivityIndices()) {
-			if (!dfg.isStartActivity(endActivity)) {
-				for (long edge : dfg.getDirectlyFollowsGraph().getIncomingEdgesOf(endActivity)) {
-					candidates[connectedComponents.get(dfg.getDirectlyFollowsGraph().getEdgeSource(edge))] = false;
-				}
-			}
-		}
-
-		//exclude all candidates that have no connection to all start activities
-		for (Integer cc = 0; cc < ccs; cc++) {
-			Set<XEventClass> end = subEndActivities.get(cc);
-			for (XEventClass node1 : end) {
-				for (XEventClass node2 : dfg.getStartActivities()) {
-					if (!dfg.getDirectlyFollowsGraph().containsEdge(node1, node2)) {
-						candidates[cc] = false;
-						//debug("body part for no connection to all start activities " + cc.toString());
-					}
-				}
-			}
-		}
-
-		//exclude all candidates that have no connection from all end activities
-		for (Integer cc = 0; cc < ccs; cc++) {
-			Set<XEventClass> start = subStartActivities.get(cc);
-			for (XEventClass node1 : start) {
-				for (XEventClass node2 : dfg.getEndActivities()) {
-					if (!dfg.getDirectlyFollowsGraph().containsEdge(node2, node1)) {
-						candidates[cc] = false;
-						//debug("body part for no connection from all end activities " + node2.toString() + " " + node1.toString());
-					}
-				}
-			}
-		}
-
-		//make the lists of sets of nodes
-		List<Set<XEventClass>> result = new ArrayList<Set<XEventClass>>();
-		for (int i = 0; i < ccs; i++) {
-			result.add(new HashSet<XEventClass>());
-		}
-
-		//divide the activities
-		for (XEventClass node : dfg.getDirectlyFollowsGraph().getVertices()) {
-			//debug(node.toString() + " in connected component " + connectedComponents.get(node));
-			int index;
-			if (candidates[connectedComponents.get(node)]) {
-				index = connectedComponents.get(node);
-				//debug(", redo part of loop");
-			} else {
-				index = 0;
-				//debug(", body part of loop");
-			}
-			Set<XEventClass> s = result.get(index);
-			s.add(node);
-			result.set(index, s);
-		}
-
-		//filter the empty sets
-		List<Set<XEventClass>> result2 = new ArrayList<Set<XEventClass>>();
-		for (Set<XEventClass> set : result) {
-			if (set.size() > 0) {
-				result2.add(set);
-			}
-		}
-
-		//debug(result2.toString());
-		return new Cut(Operator.loop, result2);
-	}
-
-	private static void labelConnectedComponents(Graph<XEventClass> graph, XEventClass node,
-			Map<XEventClass, Integer> connectedComponents, Integer connectedComponent) {
-		if (!connectedComponents.containsKey(node)) {
-			connectedComponents.put(node, connectedComponent);
-			for (long edge : graph.getEdgesOf(node)) {
-				labelConnectedComponents(graph, graph.getEdgeSource(edge), connectedComponents, connectedComponent);
-				labelConnectedComponents(graph, graph.getEdgeTarget(edge), connectedComponents, connectedComponent);
-			}
-		}
 	}
 
 }
