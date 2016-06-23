2523a65e72d564b041ec75b00352df9f67497797
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2016-06-23 14:40:30 +0000

    trace miner uses sets

--- src/org/processmining/plugins/flowerMiner/TraceMiner.java
+++ src/org/processmining/plugins/flowerMiner/TraceMiner.java
@@ -3,8 +3,12 @@ package org.processmining.plugins.flowerMiner;
 import gnu.trove.iterator.TObjectIntIterator;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.map.TObjectIntMap;
+import gnu.trove.set.hash.TCustomHashSet;
+import gnu.trove.strategy.HashingStrategy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 
 import org.deckfour.xes.classification.XEventClassifier;
 import org.deckfour.xes.model.XEvent;
@@ -34,26 +38,57 @@ public class TraceMiner {
 		final TObjectIntMap<String> activity2int = EfficientTree.getEmptyActivity2int();
 		final ArrayList<String> int2activity = new ArrayList<>();
 		final TIntArrayList tree = new TIntArrayList();
-		tree.add(EfficientTree.xor - log.size() * EfficientTree.childrenFactor);
+		tree.add(EfficientTree.xor);
+
+		Set<int[]> set = new TCustomHashSet<int[]>(new HashingStrategy<int[]>() {
+
+			private static final long serialVersionUID = 5948289403059749878L;
+
+			public int computeHashCode(int[] object) {
+				return Arrays.hashCode(object);
+			}
+
+			public boolean equals(int[] o1, int[] o2) {
+				return Arrays.equals(o1, o2);
+			}
+		});
 
 		//parse the traces
 		for (XTrace trace : log) {
+			int[] iTrace = new int[trace.size()];
+			int i = 0;
+			for (XEvent event : trace) {
+				String activity = classifier.getClassIdentity(event);
+				int actIndex = activity2int.putIfAbsent(activity, int2activity.size());
+				if (actIndex == activity2int.getNoEntryValue()) {
+					iTrace[i] = int2activity.size();
+					int2activity.add(activity);
+				} else {
+					iTrace[i] = actIndex;
+				}
+				i++;
+			}
 
-			//start the trace
-			if (trace.isEmpty()) {
-				tree.add(EfficientTree.tau);
-			} else {
-				tree.add(EfficientTree.sequence - trace.size() * EfficientTree.childrenFactor);
-				for (XEvent event : trace) {
-					String activity = classifier.getClassIdentity(event);
-					int actIndex = activity2int.putIfAbsent(activity, int2activity.size());
-					if (actIndex == activity2int.getNoEntryValue()) {
-						tree.add(int2activity.size());
-						int2activity.add(activity);
-					} else {
-						tree.add(actIndex);
+			if (!set.contains(iTrace)) {
+				set.add(iTrace);
+
+				//start the trace
+				if (trace.isEmpty()) {
+					tree.add(EfficientTree.tau);
+				} else {
+					tree.add(EfficientTree.sequence - trace.size() * EfficientTree.childrenFactor);
+					for (XEvent event : trace) {
+						String activity = classifier.getClassIdentity(event);
+						int actIndex = activity2int.putIfAbsent(activity, int2activity.size());
+						if (actIndex == activity2int.getNoEntryValue()) {
+							tree.add(int2activity.size());
+							int2activity.add(activity);
+						} else {
+							tree.add(actIndex);
+						}
 					}
 				}
+				tree.set(0, tree.get(0) - EfficientTree.childrenFactor);
 			}
 		}
 
