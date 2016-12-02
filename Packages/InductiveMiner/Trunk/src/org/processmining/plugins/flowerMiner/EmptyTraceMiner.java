9860f28f69da5600980b25b698221f54d58e07b7
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2016-12-02 07:19:03 +0000

    added tau miner

---
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/flowerMiner/EmptyTraceMiner.java
@@ -0,0 +1,30 @@
+package org.processmining.plugins.flowerMiner;
+
+import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginLevel;
+import org.processmining.framework.plugin.annotations.PluginVariant;
+import org.processmining.processtree.Node;
+import org.processmining.processtree.ProcessTree;
+import org.processmining.processtree.impl.AbstractTask;
+import org.processmining.processtree.impl.ProcessTreeImpl;
+
+@Plugin(name = "Mine Process tree using Tau Miner", returnLabels = { "Process Tree" }, returnTypes = {
+		ProcessTree.class }, parameterLabels = {}, userAccessible = true, level = PluginLevel.Regular)
+public class EmptyTraceMiner {
+
+	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "S.J.J. Leemans", email = "s.j.j.leemans@tue.nl")
+	@PluginVariant(variantLabel = "Mine a flower Petri net", requiredParameterLabels = {})
+	public ProcessTree mine(PluginContext context) {
+		return mine();
+	}
+
+	public static ProcessTree mine() {
+		ProcessTree tree = new ProcessTreeImpl();
+		Node root = new AbstractTask.Automatic("tau");
+		root.setProcessTree(tree);
+		tree.setRoot(root);
+		return tree;
+	}
+}
