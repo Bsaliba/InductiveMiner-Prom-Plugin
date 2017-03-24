c4c3b81d4e4983fb86e1013725eeb1c11c62b00c
sleemans <sleemans@a68f48d4-8d00-424f-b812-3f22a534a02a>
2017-03-24 00:53:12 +0000

    add reduction rule that reduces xor(a, a) to xor(a)

---
+++ Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/efficienttree/reductionrules/XorDoubleSingleChild.java
@@ -0,0 +1,30 @@
+package org.processmining.plugins.InductiveMiner.efficienttree.reductionrules;
+
+import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTree;
+import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTreeReductionRule;
+import org.processmining.plugins.InductiveMiner.efficienttree.UnknownTreeNodeException;
+
+public class XorDoubleSingleChild implements EfficientTreeReductionRule {
+
+	public boolean apply(EfficientTree tree, int node) throws UnknownTreeNodeException {
+		if (tree.isXor(node)) {
+			boolean[] activitiesSeen = new boolean[tree.getInt2activity().length];
+
+			for (int child : tree.getChildren(node)) {
+				if (tree.isActivity(child)) {
+					int activity = tree.getActivity(child);
+					if (!activitiesSeen[activity]) {
+						activitiesSeen[activity] = true;
+					} else {
+						//remove this activity
+						tree.removeChild(node, child);
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+}
